091babecd66364f90d842b54608048b0a0f63cce
fox <fox@coolbe.com.tw>
2017-12-20 10:40:25 +0800

    add Logger.php and test write log

---
+++ Logger.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * For separator log file.
+ */
+class Logger
+{
+    const LOG_LEVEL_INFO = 'INFO';
+    const LOG_LEVEL_DEBUG = 'DEBUG';
+    const LOG_LEVEL_WARN = 'WARN';
+    const LOG_LEVEL_ERROR = 'ERROR';
+    const LOG_LEVEL_FATAL = 'FATAL';
+
+    const LOG_LEVEL_INFO_NUM = 4;
+    const LOG_LEVEL_DEBUG_NUM = 3;
+    const LOG_LEVEL_WARN_NUM = 2;
+    const LOG_LEVEL_ERROR_NUM = 1;
+    const LOG_LEVEL_FATAL_NUM = 0;
+
+    const LOG_FORMAT = "%s.%06d|%5s|%6d|%s\n";
+    const DATE_FORMAT = 'Y-m-d H:i:s';
+    const TIMEZONE = 'Asia/Taipei';
+
+    private $fileName;
+    private $logLevelNum;
+    private $pid;
+
+    /**
+     * constructor.
+     *
+     * @param string $fileName log file prefix name
+     */
+    public function __construct($fileName)
+    {
+        $CI = &get_instance();
+        $this->logLevelNum = $CI->config->item('log_threshold');
+        $this->fileName = APPPATH.'logs/'.$fileName.'.%s.log';
+        $this->pid = getmypid();
+		//$this->pid = 1;
+    }
+
+    /**
+     * info level.
+     *
+     * @param string $content log content
+     *
+     * @return
+     */
+    public function info($content)
+    {
+        if ($this->logLevelNum >= self::LOG_LEVEL_INFO_NUM) {
+            $this->writeLog(self::LOG_LEVEL_INFO, $content);
+        }
+    }
+
+    /**
+     * debug level.
+     *
+     * @param string $content log content
+     *
+     * @return
+     */
+    public function debug($content)
+    {
+        if ($this->logLevelNum >= self::LOG_LEVEL_DEBUG_NUM) {
+            $this->writeLog(self::LOG_LEVEL_DEBUG, $content);
+        }
+    }
+
+    /**
+     * warn level.
+     *
+     * @param string $content log content
+     *
+     * @return
+     */
+    public function warn($content)
+    {
+        if ($this->logLevelNum >= self::LOG_LEVEL_WARN_NUM) {
+            $this->writeLog(self::LOG_LEVEL_WARN, $content);
+        }
+    }
+
+    /**
+     * error level.
+     *
+     * @param string $content log content
+     *
+     * @return
+     */
+    public function error($content)
+    {
+        if ($this->logLevelNum >= self::LOG_LEVEL_ERROR_NUM) {
+            $this->writeLog(self::LOG_LEVEL_ERROR, $content);
+        }
+    }
+
+    /**
+     * fatal levvel.
+     *
+     * @param string $content log content
+     *
+     * @return
+     */
+    public function fatal($content)
+    {
+        $this->writeLog(self::LOG_LEVEL_FATAL, $content);
+    }
+
+    private function writeLog($level, $content)
+    {
+		$microtime = sprintf("%06d", explode(" ", microtime())[0] * 1000000);
+		$fname = sprintf($this->fileName, date('Y-m-d'));
+        $content = sprintf(self::LOG_FORMAT,
+             date(self::DATE_FORMAT), $microtime, $level, $this->pid, $content);
+        file_put_contents($fname, $content, FILE_APPEND | LOCK_EX);
+    }
+}
