442ca51615a528c6fc58ed3c44ab865e299c57e0
fox <fox@coolbe.com.tw>
2017-12-20 16:24:54 +0800

    update code

--- Webhook.php
+++ Webhook.php
@@ -1,9 +1,35 @@
 <?php 
+require __DIR__ . '/vendor/autoload.php';
 
 require_once './Logger.php';
-
+require_once './TestCase/ReplyMessage.php';
+$accessToken = "WKbusDT+cnQi8JxsfTTuIT13WlvCpcaQ1X3Ye0Rk3UEL6stWaCsdKAU3xF4MNyrUeeXx/Gi9KqH1M3gzQTkcGSRWHFsyTlURlQXLQOf7u3BfLtUN6zygSXGm/LWPUofb5fXMK8i3L5Yq4ZzMbiOD8Y9PbdgDzCFqoOLOYbqAITQ=";
+$channelSecret = "f9fa44d9685ba1156830641eb411529e";
+$channelId = "1545665346";
 $log = new Logger('Webhook');
 
-$log->info(file_get_contents("php://input"));
+$log->info('RECEIVE |'.file_get_contents("php://input"));
+$postData = json_decode(file_get_contents("php://input"));
+print_r($postData);
+
+if( !empty($postData->events) ) {
+    switch ($postData->events[0]->type) {
+        case "message":
+            ReplyMessage::doTest($postData->events[0], $accessToken, $channelSecret);
+            break;
+        case "follow":
+            break;
+        case "unfollow":
+            break;
+        case "join":
+            break;
+        case "leave":
+            break;
+        case "postback":
+            break;
+        case "beacon":
+            break;
+    }
+}
 
 ?>
